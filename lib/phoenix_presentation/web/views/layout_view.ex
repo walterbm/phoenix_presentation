65105016cbedfb1f88e91f665a29de8db8cc7482
Walter Beller-Morales <walter.beller.morales@gmail.com>
2017-07-05 02:10:18 -0400

    modify layout view to load page specific css

--- lib/phoenix_presentation/web/views/layout_view.ex
+++ lib/phoenix_presentation/web/views/layout_view.ex
@@ -1,3 +1,11 @@
 defmodule PhoenixPresentation.Web.LayoutView do
   use PhoenixPresentation.Web, :view
+
+  def view_name(conn) do
+    conn
+    |> view_module
+    |> Phoenix.Naming.resource_name
+    |> String.replace("_view", "")
+  end
+
 end
