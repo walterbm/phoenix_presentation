9848804c0da8c0de84b235d4bdd873acaa5a4660
Walter Beller-Morales <walter.beller.morales@gmail.com>
2017-07-06 06:06:34 -0400

    fix slide path to not be set during build phase

--- lib/phoenix_presentation/web/controllers/presentation_controller.ex
+++ lib/phoenix_presentation/web/controllers/presentation_controller.ex
@@ -3,11 +3,12 @@ defmodule PhoenixPresentation.Web.PresentationController do
 
   alias PhoenixPresentation.ContentParser.Markdown
 
-  @slides Path.join(:code.priv_dir(:phoenix_presentation), "static/slides.md")
+  @slides "static/slides.md"
 
   def index(conn, params) do
+    slide_path = Path.join(:code.priv_dir(:phoenix_presentation, @slides))
     number = Map.get(params, "slide", "0") |> String.to_integer
-    slide = Markdown.convert_slide(@slides, number)
+    slide = Markdown.convert_slide(slide_path, number)
     render conn, "slide.html", slide: slide
   end
 
